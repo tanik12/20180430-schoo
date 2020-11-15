f07a0922924dc6eb421270f10acfe1179074e481
tanik12 <34958079+tanik12@users.noreply.github.com>
2020-11-15 17:57:06 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock "~> 3.10.2"
 #
 # あなたのGitHubユーザー名に変更してください
 #
-set :repo_url, "https://github.com/xxxx/20180430-schoo.git"
+set :repo_url, "https://github.com/tanik12/20180430-schoo.git"
 
 #
 # あなたのWebサーバーの環境に合わせてデプロイ先ディレクトリパスを設定してください
